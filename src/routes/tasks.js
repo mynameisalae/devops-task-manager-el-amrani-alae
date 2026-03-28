6c6b7982d91b5171b06c9ad45d8b147db98b399c
mynameisalae <alae.elamrami@gmail.com>
2026-03-28 01:22:47 +0100

    add the POST method to the tasks.js

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -9,4 +9,16 @@ const tasks = [
 router.get('/', (req, res) => {
  res.json(tasks);
 }); 
+
+router.post('/', (req, res) => {
+  const newTask = {
+    id: tasks.length + 1,
+    title: req.body.title,
+    completed: req.body.completed
+  };
+
+  tasks.push(newTask);
+  res.status(201).json(newTask);
+});
+
 module.exports = router; 
